07315e31ff16350a937ebd94723aaff7134464a0
24DP3MVigu <a242491mv@rvt.lv>
2025-09-18 14:25:42 +0300

    Pirmais_Darbs

---
+++ src/main/java/rvt/Pirmais_darbs.java
@@ -0,0 +1,12 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Pirmais_darbs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a something:");
+        String message = scanner.nextLine();
+        System.out.println(message);
+    }
+}
